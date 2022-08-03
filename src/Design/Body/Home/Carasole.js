aef667d159599d7243c6cb546fe957bdc1973692
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-03 23:33:46 +0530

    button set created

--- src/Design/Body/Home/Carasole.js
+++ src/Design/Body/Home/Carasole.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import img1 from "./a.jpg";
+import img2 from "./b.jpg";
+import img3 from "./c.jpg";
+import img4 from "./d.jpg";
 
+const images =[img3,img2,img1,img4];
 export default function Carasole() {
+  const [img, setImg] = React.useState(0);
+  setTimeout(() => {
+    setImg(img + 1);
+  }, 2000);
+  const y = img % 4;
   return (
-    <div>Carasole</div>
-  )
+    <div>
+      <img src={images[y]} style={{width:"100%"}}/>
+    </div>
+  );
 }
--- src/Design/Body/Home/Home.js
+++ src/Design/Body/Home/Home.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import ButtonSet from "./ButtonSet";
+import Carasole from "./Carasole";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Carasole />
+      <i>
+        <p
+          id="para"
+          style={{
+            textAlign: "center",
+            fontSize: "30px",
+            fontWeight: "600",
+          }}
+        >
+          {" "}
+          New Ideas
+        </p>
+      </i>
+      <ButtonSet />
+      
+    </div>
+  );
 }
