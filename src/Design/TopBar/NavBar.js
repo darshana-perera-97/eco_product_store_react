8fe2812735976e33df18d861ec72d4c79bdc0568
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-03 09:57:00 +0530

    navbar links created

--- src/Design/TopBar/NavBar.js
+++ src/Design/TopBar/NavBar.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <div style={{ paddingTop: "10px" }}>
+      <Router>
+        <nav>
+          <Link to="/" id="link">
+            Home
+          </Link>
+          <Link to="/store" id="link">
+            Store
+          </Link>
+          <Link to="/ides" id="link">
+            Ideas
+          </Link>
+          <Link to="/about" id="link">
+            About
+          </Link>
+        </nav>
+      </Router>
+    </div>
+  );
 }
