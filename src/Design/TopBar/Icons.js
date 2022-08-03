e675e064a7cb77888bad2afc056f89bfa9a92703
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-03 10:07:23 +0530

    topbar created

--- src/Design/TopBar/Icons.js
+++ src/Design/TopBar/Icons.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import AutoAwesomeMosaicIcon from "@mui/icons-material/AutoAwesomeMosaic";
+import MailIcon from "@mui/icons-material/Mail";
+import PersonPinIcon from "@mui/icons-material/PersonPin";
 
 export default function Icons() {
   return (
-    <div>Icons</div>
-  )
+    <div>
+      <AutoAwesomeMosaicIcon id="icn" />
+      <MailIcon id="icn" />
+      <PersonPinIcon id="icn" />
+    </div>
+  );
 }
