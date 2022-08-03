33803c698f94f83cf6a9275901c32ee52508a090
Darshana Perera <darshana.saluka.10@gmail.com>
2022-08-03 09:24:52 +0530

    topBar structure created

---
+++ src/Design/TopBar/TopBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Icons from "./Icons";
+import Logo from "./Logo";
+import NavBar from "./NavBar";
+
+export default function TopBar() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "10px 80px",
+      }}
+    >
+      <div style={{ display: "flex" }}>
+        <Logo />
+        <NavBar />
+      </div>
+      <Icons />
+    </div>
+  );
+}
